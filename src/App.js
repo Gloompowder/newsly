08348b51a37d73ebb66dbd4b36703006f03d614b
William <willlin1996@gmail.com>
2020-06-05 10:32:03 -0400

    saved

--- src/App.js
+++ src/App.js
@@ -10,26 +10,73 @@ class App extends React.Component {
 
   state = {
     articles: articles,
+    view: 'cards',
+    theme: 'light'
+  }
+
+
+  handleLightDarkClick = () =>{
+    switch(
+      this.state.theme
+    ){
+      case 'light':
+        this.setState(
+          {theme: 'dark'}
+        )
+        break
+      case 'dark':
+        this.setState(
+          {theme: 'light'}
+        )
+        break
+        default: this.setState({theme: 'light'})
+    }
+  }
+
+  handleCardListClick = () =>{
+    switch(
+      this.state.view
+    ){
+      case 'cards':
+        this.setState(
+          {view: 'list'}
+        )
+        break
+      case 'list':
+        this.setState(
+          {view: 'cards'}
+        )
+        break
+        default: this.setState({view: 'cards'})
+    }
   }
 
   renderArticles = () => {
     return this.state.articles.map(article => {
-      return <ArticleCard 
-              key={article.id} 
-              title={article.title}
-              url={article.url}
-              urlToImage={article.urlToImage}
-              description={article.description}
-              />
+              if(this.state.view === 'list'){
+                return <ArticleItem 
+                key = {article.id}
+                title = {article.title}
+                theme = {this.state.theme}/>
+              }else if(this.state.view === 'cards'){
+                return <ArticleCard 
+                key={article.id} 
+                title={article.title}
+                url={article.url}
+                urlToImage={article.urlToImage}
+                description={article.description}
+                theme = {this.state.theme}
+                />
+              }
     })
   }
 
   render(){
     return (
-      <div className="light">
-        <button>Switch to Dark Mode</button>
-        <button>Switch to List View</button>
-        <div className="cards">
+      <div className={this.state.theme}>
+        <button onClick={this.handleLightDarkClick}>Switch to {this.state.theme.charAt(0).toUpperCase() + this.state.theme.slice(1) } Mode</button>
+        <button onClick={this.handleCardListClick}>Switch to {this.state.view.charAt(0).toUpperCase() + this.state.view.slice(1) } View</button>
+        <div className={this.state.view}>
           {this.renderArticles()}
         </div>
       </div>
